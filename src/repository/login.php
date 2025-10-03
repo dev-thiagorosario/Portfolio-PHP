2dc9fcb8ab259738109807410fe730d055c2ae4e
Thiago Rosario <eng.thiagosouza@icloud.com>
2025-10-03 18:57:33 -0300

    Update admin login redirect to new admin panel location

--- src/repository/login.php
+++ src/repository/login.php
@@ -2,7 +2,7 @@
 
 function encaminharUsuario($user) {
     if ($user['id'] === 1 || $user['id'] === 2) {
-        header('Location: /pages/home-admin.php');
+        header('Location: /pages/admin/home-admin.php');
         exit();
     } else {
         header('Location: /pages/home.php');
